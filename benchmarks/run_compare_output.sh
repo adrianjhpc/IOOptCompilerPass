4a3f61ea678666108ed9ca15392fe2c9475707d6
Adrian Jackson <a.jackson@epcc.ed.ac.uk>
2026-03-05 10:19:26 +0000

    Updating runner script for the benchmarks

--- benchmarks/run_compare_output.sh
+++ benchmarks/run_compare_output.sh
@@ -1,6 +1,6 @@
 python3 compare_io.py --out-file streaming_output.log benchmarks/media_standard benchmarks/media_fast 
 python3 compare_io.py --out-file wal_output.log benchmarks/wal_standard benchmarks/wal_fast 
-python3 compare_io.py --out-file db benchmarks/wal_standard benchmarks/wal_fast 
 python3 compare_io.py --out-file http_output.log benchmarks/http_standard benchmarks/http_fast 
+python3 compare_io.py --out-file lto_benchmark.log benchmarks/lto_standard benchmarks/lto_fast 
 python3 compare_io.py --out-file output.ppm benchmarks/pixel_write_standard benchmarks/pixel_write_fast 
 
